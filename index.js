3fcc6252cba658b08a2cf80bc2e35fc90b040f22
Radio <152258403+Elesovitost@users.noreply.github.com>
2024-02-28 18:47:47 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-      function changePage(url) {
-        document.getElementById("myIframe").src = url;
-      }
-      
-      window.onload = function() {
-        document.getElementById("myIframe").src = "FDGPET.html";
-        appendTextFromFDGPET();
-      };
