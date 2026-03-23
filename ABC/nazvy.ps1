7ae202f859258e2ed30e729bdd05cf3a87f491f8
Radio <ielesreg@gmail.com>
2026-03-23 22:06:48 +0100

    delete ABC

--- ABC/nazvy.ps1
+++
@@ -1,4 +0,0 @@
-$files = Get-ChildItem -File | Where-Object { $_.Extension -match '^\.(jpg|jpeg)$' }
-$names = $files | Select-Object -ExpandProperty BaseName | ForEach-Object { "'$_'" }
-$jsArray = "const words = [" + ($names -join ", ") + "];"
-Set-Content -Path "nazvy.txt" -Value $jsArray
